bd0a0f630c501c850a76b498601113e669f84593
ChandanGC <111351968+ChandanGC@users.noreply.github.com>
2023-11-24 18:36:00 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -42,3 +42,27 @@ module.exports = function (config) {
     restartOnFileChange: true
   });
 };
+const puppeteer = require('puppeteer');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['CustomChrome'],
+    customLaunchers: {
+      CustomChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    // other configurations...
+  });
+
+  config.set({
+    customLaunchers: {
+      PuppeteerChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    }
+  });
+};
+
